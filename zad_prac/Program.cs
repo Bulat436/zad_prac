b7b82a9ba278671e76c6eedf5de0d3d3d73e9af6
gafia <gafiatov01@gmail.com>
2025-03-18 21:11:09 +0300

    Решение первого задания

--- zad_prac/Program.cs
+++ zad_prac/Program.cs
@@ -10,6 +10,28 @@ namespace zad_prac
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите строку:");
+            string row = Console.ReadLine();
+
+            string result;
+
+            if (row.Length % 2 == 0)
+            {
+                int mid = row.Length / 2;
+                string firsthalf = row.Substring(0, mid);
+                string secondhalf = row.Substring(mid);
+                string reversfirsthalf = new string(firsthalf.Reverse().ToArray());
+                string reverssecondhalf = new string(secondhalf.Reverse().ToArray());
+                result = reversfirsthalf + reverssecondhalf;
+            }
+            else
+            {
+                string reversed = new string(row.Reverse().ToArray());
+                result = reversed + row;
+            }
+
+            Console.WriteLine("Обработанная строка:");
+            Console.WriteLine(result);
         }
     }
 }
