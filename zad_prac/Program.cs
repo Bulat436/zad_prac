4917123047e30acd4f55d5ecfeb38f7ea395c232
gafia <gafiatov01@gmail.com>
2025-04-07 21:59:11 +0300

    Задание 4

--- zad_prac/Program.cs
+++ zad_prac/Program.cs
@@ -14,10 +14,8 @@ namespace zad_prac
             string row = Console.ReadLine();
 
             string result;
-            if (LowercaseEnglish(row))
+            if (LowercaseEnglish(row) & ContainsAEIOUY(row))
             {
-
-
                 if (row.Length % 2 == 0)
                 {
                     int mid = row.Length / 2;
@@ -34,29 +32,34 @@ namespace zad_prac
                 }
                 Console.WriteLine("Обработанная строка:");
                 Console.WriteLine(result);
+
+                Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+                foreach (char c in row)
+                {
+                    if (letterCount.ContainsKey(c))
+                    {
+                        letterCount[c]++;
+                    }
+                    else
+                    {
+                        letterCount[c] = 1;
+                    }
+                }
+                Console.WriteLine("Количество каждой буквы:");
+                foreach (var item in letterCount)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+                string subs = LongestSubstring(result);
+                Console.WriteLine("Самая длинная подстрока:");
+                Console.WriteLine(subs);
             }
             else
             {
                 Console.WriteLine("Неправильный ввод");
             }
-            Dictionary<char, int> letterCount = new Dictionary<char, int>();
 
-            foreach (char c in row)
-            {
-                if (letterCount.ContainsKey(c))
-                {
-                    letterCount[c]++;
-                }
-                else
-                {
-                    letterCount[c] = 1;
-                }
-            }
-            Console.WriteLine("Количество каждой буквы:");
-            foreach (var item in letterCount)
-            {
-                Console.WriteLine($"{item.Key}: {item.Value}");
-            }
         }
         static bool LowercaseEnglish(string row)
         {
@@ -68,6 +71,35 @@ namespace zad_prac
                 }
             }
             return true;
+        }
+        static bool ContainsAEIOUY(string str)
+        {
+            return str.Contains('a') || str.Contains('e') || str.Contains('i') || str.Contains('o') || str.Contains('u') || str.Contains('y');
+        }
+        static string LongestSubstring(string str)
+        {
+            string let = "aeiouy";
+            string longestSubstring = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (let.Contains(str[i]))
+                {
+                    for (int j = str.Length - 1; j > i; j--)
+                    {
+                        if (let.Contains(str[j]))
+                        {
+                            string substring = str.Substring(i, j - i + 1);
+                            if (substring.Length > longestSubstring.Length)
+                            {
+                                longestSubstring = substring;
+                            }
+                            break; 
+                        }
+                    }
+                }
+            }
+            return longestSubstring;
         }
     }
 }
